c6d8e0c8ff4794487c97dfd6efa9594d9a5968e8
nanowebcoder <kcasey@nanoweb.com>
2016-03-06 23:43:05 -0500

    Added files via upload

---
+++ DeviceEditor/frmMain.Designer.cs
@@ -0,0 +1,375 @@
+﻿namespace DeviceEditor
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.devicesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnLoadDevices = new System.Windows.Forms.Button();
+            this.txtApiURL = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.gridDevices = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtContentBody = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtHttpVerb = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtContentType = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDeviceType = new System.Windows.Forms.TextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtOffUrl = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtOnUrl = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtDeviceName = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtDeviceId = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.devicesBindingSource)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridDevices)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // devicesBindingSource
+            // 
+            this.devicesBindingSource.DataSource = typeof(VeraHuesBridge.Devices);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.btnLoadDevices);
+            this.panel1.Controls.Add(this.txtApiURL);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(703, 40);
+            this.panel1.TabIndex = 37;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 16);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 13);
+            this.label6.TabIndex = 39;
+            this.label6.Text = "URL:";
+            // 
+            // btnLoadDevices
+            // 
+            this.btnLoadDevices.Location = new System.Drawing.Point(338, 12);
+            this.btnLoadDevices.Name = "btnLoadDevices";
+            this.btnLoadDevices.Size = new System.Drawing.Size(47, 21);
+            this.btnLoadDevices.TabIndex = 38;
+            this.btnLoadDevices.Text = "Load";
+            this.btnLoadDevices.UseVisualStyleBackColor = true;
+            this.btnLoadDevices.Click += new System.EventHandler(this.btnLoadDevices_Click);
+            // 
+            // txtApiURL
+            // 
+            this.txtApiURL.Location = new System.Drawing.Point(49, 13);
+            this.txtApiURL.Name = "txtApiURL";
+            this.txtApiURL.Size = new System.Drawing.Size(286, 20);
+            this.txtApiURL.TabIndex = 37;
+            this.txtApiURL.Text = "http://10.10.1.26:8080/api/devices";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 40);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.gridDevices);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.label3);
+            this.splitContainer1.Panel2.Controls.Add(this.txtContentBody);
+            this.splitContainer1.Panel2.Controls.Add(this.label4);
+            this.splitContainer1.Panel2.Controls.Add(this.txtHttpVerb);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.txtContentType);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Controls.Add(this.txtDeviceType);
+            this.splitContainer1.Panel2.Controls.Add(this.btnDelete);
+            this.splitContainer1.Panel2.Controls.Add(this.btnUpdate);
+            this.splitContainer1.Panel2.Controls.Add(this.btnAdd);
+            this.splitContainer1.Panel2.Controls.Add(this.label10);
+            this.splitContainer1.Panel2.Controls.Add(this.txtOffUrl);
+            this.splitContainer1.Panel2.Controls.Add(this.label9);
+            this.splitContainer1.Panel2.Controls.Add(this.txtOnUrl);
+            this.splitContainer1.Panel2.Controls.Add(this.label8);
+            this.splitContainer1.Panel2.Controls.Add(this.txtDeviceName);
+            this.splitContainer1.Panel2.Controls.Add(this.label7);
+            this.splitContainer1.Panel2.Controls.Add(this.txtDeviceId);
+            this.splitContainer1.Size = new System.Drawing.Size(703, 664);
+            this.splitContainer1.SplitterDistance = 277;
+            this.splitContainer1.SplitterWidth = 10;
+            this.splitContainer1.TabIndex = 39;
+            // 
+            // gridDevices
+            // 
+            this.gridDevices.AllowUserToAddRows = false;
+            this.gridDevices.AllowUserToDeleteRows = false;
+            this.gridDevices.AutoGenerateColumns = false;
+            this.gridDevices.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.gridDevices.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridDevices.DataSource = this.devicesBindingSource;
+            this.gridDevices.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridDevices.Location = new System.Drawing.Point(0, 0);
+            this.gridDevices.Name = "gridDevices";
+            this.gridDevices.Size = new System.Drawing.Size(703, 277);
+            this.gridDevices.TabIndex = 20;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 62;
+            this.label3.Text = "Content Body";
+            // 
+            // txtContentBody
+            // 
+            this.txtContentBody.Location = new System.Drawing.Point(86, 190);
+            this.txtContentBody.Multiline = true;
+            this.txtContentBody.Name = "txtContentBody";
+            this.txtContentBody.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtContentBody.Size = new System.Drawing.Size(585, 138);
+            this.txtContentBody.TabIndex = 61;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 60;
+            this.label4.Text = "httpVerb";
+            // 
+            // txtHttpVerb
+            // 
+            this.txtHttpVerb.Location = new System.Drawing.Point(86, 164);
+            this.txtHttpVerb.Name = "txtHttpVerb";
+            this.txtHttpVerb.Size = new System.Drawing.Size(585, 20);
+            this.txtHttpVerb.TabIndex = 59;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 56;
+            this.label2.Text = "Content Type";
+            // 
+            // txtContentType
+            // 
+            this.txtContentType.Location = new System.Drawing.Point(86, 138);
+            this.txtContentType.Name = "txtContentType";
+            this.txtContentType.Size = new System.Drawing.Size(585, 20);
+            this.txtContentType.TabIndex = 55;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 54;
+            this.label1.Text = "Device Type";
+            // 
+            // txtDeviceType
+            // 
+            this.txtDeviceType.Location = new System.Drawing.Point(86, 112);
+            this.txtDeviceType.Name = "txtDeviceType";
+            this.txtDeviceType.Size = new System.Drawing.Size(585, 20);
+            this.txtDeviceType.TabIndex = 53;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(608, 334);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(59, 24);
+            this.btnDelete.TabIndex = 52;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(543, 334);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(59, 24);
+            this.btnUpdate.TabIndex = 51;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(478, 334);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(59, 24);
+            this.btnAdd.TabIndex = 50;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(48, 89);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(32, 13);
+            this.label10.TabIndex = 49;
+            this.label10.Text = "offUrl";
+            // 
+            // txtOffUrl
+            // 
+            this.txtOffUrl.Location = new System.Drawing.Point(86, 86);
+            this.txtOffUrl.Name = "txtOffUrl";
+            this.txtOffUrl.Size = new System.Drawing.Size(585, 20);
+            this.txtOffUrl.TabIndex = 48;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(48, 63);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(32, 13);
+            this.label9.TabIndex = 47;
+            this.label9.Text = "onUrl";
+            // 
+            // txtOnUrl
+            // 
+            this.txtOnUrl.Location = new System.Drawing.Point(86, 60);
+            this.txtOnUrl.Name = "txtOnUrl";
+            this.txtOnUrl.Size = new System.Drawing.Size(585, 20);
+            this.txtOnUrl.TabIndex = 46;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(48, 37);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 45;
+            this.label8.Text = "Name";
+            // 
+            // txtDeviceName
+            // 
+            this.txtDeviceName.Location = new System.Drawing.Point(86, 34);
+            this.txtDeviceName.Name = "txtDeviceName";
+            this.txtDeviceName.Size = new System.Drawing.Size(585, 20);
+            this.txtDeviceName.TabIndex = 44;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(62, 11);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(18, 13);
+            this.label7.TabIndex = 43;
+            this.label7.Text = "ID";
+            // 
+            // txtDeviceId
+            // 
+            this.txtDeviceId.Location = new System.Drawing.Point(86, 8);
+            this.txtDeviceId.Name = "txtDeviceId";
+            this.txtDeviceId.ReadOnly = true;
+            this.txtDeviceId.Size = new System.Drawing.Size(585, 20);
+            this.txtDeviceId.TabIndex = 42;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 704);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Nano Vera/Hues Bridge Device Editor";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.devicesBindingSource)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridDevices)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource devicesBindingSource;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnLoadDevices;
+        private System.Windows.Forms.TextBox txtApiURL;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView gridDevices;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtOffUrl;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtOnUrl;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtDeviceName;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtDeviceId;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtContentType;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDeviceType;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtHttpVerb;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtContentBody;
+    }
+}
+
---
+++ DeviceEditor/frmMain.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net;
+using System.Net.Http.Headers;
+using VeraHuesBridge;
+
+namespace DeviceEditor
+{
+    public partial class frmMain : Form
+    {
+        //public List<VeraHuesBridge.Device> deviceList;
+        public BindingList<VeraHuesBridge.Device> deviceList;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+         private void BindObjects()
+        {
+            devicesBindingSource.DataSource = this.deviceList;
+
+            gridDevices.AutoGenerateColumns = true;
+
+            txtDeviceName.DataBindings.Clear();
+            txtDeviceId.DataBindings.Clear();
+            txtOffUrl.DataBindings.Clear();
+            txtOnUrl.DataBindings.Clear();
+
+            txtDeviceType.DataBindings.Clear();
+            txtContentType.DataBindings.Clear();
+            txtHttpVerb.DataBindings.Clear();
+            txtContentBody.DataBindings.Clear();
+
+            txtDeviceName.DataBindings.Add(new Binding("Text", devicesBindingSource, "name"));
+            txtDeviceId.DataBindings.Add(new Binding("Text", devicesBindingSource, "id"));
+            txtOffUrl.DataBindings.Add(new Binding("Text", devicesBindingSource, "offUrl"));
+            txtOnUrl.DataBindings.Add(new Binding("Text", devicesBindingSource, "onUrl"));
+
+            txtDeviceType.DataBindings.Add(new Binding("Text", devicesBindingSource, "deviceType"));
+            txtContentType.DataBindings.Add(new Binding("Text", devicesBindingSource, "contentType"));
+            txtHttpVerb.DataBindings.Add(new Binding("Text", devicesBindingSource, "httpVerb"));
+            txtContentBody.DataBindings.Add(new Binding("Text", devicesBindingSource, "contentBody")); 
+        }
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            this.devicesBindingSource.AddingNew +=new AddingNewEventHandler(devicesBindingSource_AddingNew);
+            this.devicesBindingSource.CurrentChanged += new System.EventHandler(this.devicesBindingSource_CurrentChanged);
+
+        }
+
+        private void btnLoadDevices_Click(object sender, EventArgs e)
+        {
+            LoadData();   
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(txtApiURL.Text);
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    // New code:
+                    HttpResponseMessage response = client.GetAsync(txtApiURL.Text).Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseString = response.Content.ReadAsStringAsync().Result;
+
+                        List<VeraHuesBridge.DeviceContainer> list = Newtonsoft.Json.JsonConvert.DeserializeObject<List<VeraHuesBridge.DeviceContainer>>(responseString);
+                        this.deviceList = new BindingList<VeraHuesBridge.Device>(list[0].content);
+
+                        BindObjects();
+
+
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+
+        public void devicesBindingSource_AddingNew(object sender,AddingNewEventArgs e)
+        {
+            //VeraHuesBridge.Device device = (VeraHuesBridge.Device) e.NewObject;
+            //System.Diagnostics.Debug.WriteLine(device.name);
+            
+        }
+
+        private void devicesBindingSource_CurrentChanged(object sender, EventArgs e)
+        {
+            //VeraHuesBridge.Device device = (VeraHuesBridge.Device) devicesBindingSource.Current;
+            //System.Diagnostics.Debug.WriteLine(device.name);
+            
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Device d = (Device) devicesBindingSource.Current;
+            if (d == null ||  
+                String.IsNullOrEmpty(d.name) || 
+                String.IsNullOrEmpty(d.offUrl) || 
+                String.IsNullOrEmpty(d.onUrl) 
+                
+                )
+            {
+                MessageBox.Show("You must fill out at a minimum the Name, onUrl and offUrl.");
+                return;
+            }
+            //wipe out the id
+            d.id = null;
+
+            HttpResponseMessage response = PerformWebFunction(txtApiURL.Text, d, HttpMethod.Post);
+            if (response.IsSuccessStatusCode)
+            {
+                MessageBox.Show("Successfully Added Record!");
+                LoadData();
+            }
+            else
+            {
+                MessageBox.Show(string.Format("Failed to save record. StatusCode [{0}] ReasonPhrase [{1}]", response.StatusCode, response.ReasonPhrase));
+            }
+            
+            
+        }
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            Device device = (Device) devicesBindingSource.Current;
+            if (device == null ||  
+                String.IsNullOrEmpty(device.name) || 
+                String.IsNullOrEmpty(device.offUrl) || 
+                String.IsNullOrEmpty(device.onUrl) 
+                )
+            {
+                MessageBox.Show("You must fill out at a minimum the Name, onUrl and offUrl.");
+                return;
+            }
+
+            string url=txtApiURL.Text + "/" + device.id;
+            HttpResponseMessage response = PerformWebFunction(url, device, HttpMethod.Put);
+            if (response.IsSuccessStatusCode)
+            {
+                MessageBox.Show("Successfully Updated Record!");
+                LoadData();
+            }
+            else
+            {
+                MessageBox.Show(string.Format("Failed to update record. StatusCode [{0}] ReasonPhrase [{1}]", response.StatusCode, response.ReasonPhrase));
+            }
+
+          
+        }
+        private void btnDelete_Click(object sender, EventArgs e)
+        {            
+            Device device = (Device) devicesBindingSource.Current;
+            if (device == null ||  
+                String.IsNullOrEmpty(device.id) 
+                )
+            {
+                MessageBox.Show("You must select a device.");
+                return;
+            }
+
+            string url=txtApiURL.Text + "/" + device.id;
+            HttpResponseMessage response = PerformWebFunction(url, device, HttpMethod.Delete);
+            if (response.IsSuccessStatusCode)
+            {
+                MessageBox.Show("Successfully Deleted Record!");
+                LoadData();
+            }
+            else
+            {
+                MessageBox.Show(string.Format("Failed to delete record. StatusCode [{0}] ReasonPhrase [{1}]", response.StatusCode, response.ReasonPhrase));
+            }
+        
+        }
+
+
+        private HttpResponseMessage PerformWebFunction(string url, Device device, HttpMethod method)
+        {
+
+
+            HttpResponseMessage response = null;
+
+            using (HttpClient client = new HttpClient())
+            {
+                string deviceData = Newtonsoft.Json.JsonConvert.SerializeObject(device);
+                HttpContent content = new StringContent(deviceData, Encoding.UTF8, "application/json");
+                if (method==HttpMethod.Post)
+                {
+                    response=client.PostAsync(url, content).Result;
+                } 
+                if (method==HttpMethod.Put)
+                {
+                    response=client.PutAsync(url, content).Result;
+                } 
+                if (method==HttpMethod.Delete)
+                {
+                    response=client.DeleteAsync(url).Result;
+                } 
+                if (response==null)
+                        throw new ApplicationException("Unsupported HTTP method.");
+                }
+                
+               return response;
+            }
+
+
+
+    }
+}
